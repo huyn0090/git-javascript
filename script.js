62f22c326dab5a98fab65845a46f563e78c8a203
haihuyn0090 <henryhuynh@Henrys-MacBook-Pro.local>
2019-09-10 14:11:34 -0400

    Created Variable Number

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const name = 'Hai Huynh'
 console.log(`Helloo, my name is ${name} .`)
+
+const number = 4
+console.log(number * '2')
+console.log('8'*'5')
+console.log('40' / number)
+console.log('10'-'7')
