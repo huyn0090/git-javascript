371ab5e657cd8f8d00d3dc4fe5cb558715456a83
haihuyn0090 <henryhuynh@Henrys-MacBook-Pro.local>
2019-09-11 07:59:14 -0400

    Modified console log

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const name = 'Hai Huynh'
 console.log(`Helloo, my name is ${name} .`)
 
 const number = 4
-console.log(number * '2')
-console.log('8'*'5')
-console.log('40' / number)
-console.log('10'-'7')
+console.log((((number * '2') * '5') / number) - '7')
+
